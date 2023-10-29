d19c64bfa4d53e4009188d899f962c6912a123ca
Nick <chesinick@gmail.com>
2023-10-29 13:43:57 -0600

    Added Windows 95 page

--- customerproject/customerproject/urls.py
+++ customerproject/customerproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage
+from .views import homepage, windows95
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
+    path('windows95/', windows95, name='windows95'),
 
 ]
--- customerproject/customerproject/views.py
+++ customerproject/customerproject/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'home.html')
     #return HttpResponse("Welcome to the Microsoft Windows Museum")
+
+def windows95(request):
+    return render(request, 'windows95.html')
